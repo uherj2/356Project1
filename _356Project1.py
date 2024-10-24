5dc4c632f996e422bcdc34a2205de57c810c3a05
JackUher <jackuher@icloud.com>
2024-10-24 15:08:08 -0400

    Car stops when race is over

--- _356Project1.py
+++ _356Project1.py
@@ -10,39 +10,47 @@ class Car:
 
     #initialize mutex_lock as a static variable and as false
     mutex_lock = False
+    race_Over = False
 
     def __init__(self, row, symbol):
-
-        
         self.pos = 0
         self.row = row
         self.symbol = symbol
 
     def start(self):
-        while self.pos < 4:
+            while self.pos < 4:
 
-            #sleep timer is used to simulate busy waiting
-            time.sleep(random.randint(1, 6))
+                #sleep timer is used to detemine speed
+                #sleep timer is alo reused to simulate busy waiting
+                time.sleep(random.randint(1, 6))
 
-            if Car.mutex_lock == False:
-                #critical section
-                Car.mutex_lock = True
-                self.increase_pos()
-                Car.mutex_lock = False
-            else:
-                print(f"car {self.symbol}'s movement was momentairly blocked")
+                if Car.race_Over:
+                    print(f"Car {self.symbol} realizes the race is over")
+                    break
+
+                if Car.mutex_lock == False:
+                    Car.mutex_lock = True
+                    #critical section
+                    self.increase_pos()
+                    Car.mutex_lock = False
+                else:
+                    print(f"car {self.symbol}'s movement was momentarily blocked")
+
+            if Car.race_Over == False: 
+                Car.race_Over = True
+                print(f"game finished car {self.symbol} won the race!")
 
     def increase_pos(self):
+        #test for mutex lock: slows down process so it has the opportunity to block
+        time.sleep(1)
+
         #blank line
         print()
-
-        #test for mutex lock
-        time.sleep(random.randint(1, 3))
 
         if random.randint(1, 10) < 5:
             # change lanes
             print(f"Car {self.symbol} changes lanes")
-           
+        
             
             if self.row == 0: #checks if space is empty
                 if arr[1][self.pos] == 0: 
@@ -58,7 +66,7 @@ class Car:
                     arr[self.row][self.pos] = self.symbol
                 else:
                     print("Car",self.symbol,"tried to switch lanes but was blocked")
-           
+        
         if arr[self.row][self.pos + 1] == 0: #checks if spot ahead is empty
             arr[self.row][self.pos] = 0
             self.pos = self.pos + 1
@@ -75,8 +83,7 @@ def print_arr():
         print(row)
 
 
-# initialize 2d array
-
+# initialize 2d arrayj
 global rows, cols
 rows, cols = (2, 5)
 arr = [[0 for i in range(cols)] for j in range(rows)]
@@ -87,11 +94,8 @@ arr[1][0] = 2
 car_1 = Car(0, 1)
 car_2 = Car(1, 2)
 
-#initialize mutex lock before threading starts
-
 t1 = threading.Thread(target=car_1.start)
 t2 = threading.Thread(target=car_2.start)
 
 t1.start()
 t2.start()
-
