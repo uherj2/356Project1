5272bc95d3d375ac4495e6625a7ec77a936c67e8
Matt2-D <mattthedestroyer1234@gmail.com>
2024-10-25 12:31:43 -0400

    Add files via upload

---
+++ 356Project1.py
@@ -0,0 +1,113 @@
+from asyncio import Semaphore
+import time
+from datetime import datetime
+import random
+import threading
+
+start_time = time.time()
+
+
+class Car:
+
+    #initialize mutex_lock as a static variable and as false
+    mutex_lock = False
+    race_Over = False
+
+    def __init__(self, row, symbol):
+        self.pos = 0
+        self.row = row
+        self.symbol = symbol
+
+    def start(self):
+            while self.pos < 4:
+
+                #sleep timer is used to detemine speed
+                time.sleep(random.randint(1, 6))
+
+                if Car.race_Over:
+                    print(f"Car {self.symbol} realizes the race is over")
+                    break
+
+                if Car.mutex_lock == False:
+                    Car.mutex_lock = True
+                    #critical section
+                    self.increase_pos()
+                    Car.mutex_lock = False
+                else:
+                    print(f"\ncar {self.symbol}'s movement was momentarily blocked: busy waiting")
+                    #busy waiting checking every 0.1 seconds
+                    while(Car.mutex_lock == True):
+                        time.sleep(0.1)
+
+                    Car.mutex_lock = True
+                    #critical seciton
+                    self.increase_pos()
+                    Car.mutex_lock = False
+
+                    
+
+            if Car.race_Over == False: 
+                Car.race_Over = True
+                print(f"game finished car {self.symbol} won the race!")
+
+    def increase_pos(self):
+        #test for mutex lock: slows down process so it has the opportunity to block
+        time.sleep(1)
+
+        #blank line
+        print()
+
+        if random.randint(1, 10) < 5:
+            # change lanes
+            print(f"Car {self.symbol} changes lanes")
+        
+            
+            if self.row == 0: #checks if space is empty
+                if arr[1][self.pos] == 0: 
+                    arr[self.row][self.pos] = 0
+                    self.row = 1 #sets row to the row we just checked
+                    arr[self.row][self.pos] = self.symbol #changing the array
+                else:
+                    print("Car",self.symbol,"tried to switch lanes but was blocked")
+            else:
+                if arr[0][self.pos] == 0: 
+                    arr[self.row][self.pos] = 0
+                    self.row = 0 
+                    arr[self.row][self.pos] = self.symbol
+                else:
+                    print("Car",self.symbol,"tried to switch lanes but was blocked")
+        
+        if arr[self.row][self.pos + 1] == 0: #checks if spot ahead is empty
+            arr[self.row][self.pos] = 0
+            self.pos = self.pos + 1
+            arr[self.row][self.pos] = self.symbol
+            print(f"Car {self.symbol} moves")
+        else: 
+            print(f"Car {self.symbol} tried to move but was blocked")
+
+        print(time.time() - start_time)
+        print_arr()
+
+def print_arr():
+    for row in arr:
+        print(row)
+
+
+# initialize 2d arrayj
+global rows, cols
+rows, cols = (2, 5)
+arr = [[0 for i in range(cols)] for j in range(rows)]
+
+arr[0][0] = 1
+arr[1][0] = 2
+#obsticle
+arr[random.randint(0,1)][random.randint(1,3)]=3
+
+car_1 = Car(0, 1)
+car_2 = Car(1, 2)
+
+t1 = threading.Thread(target=car_1.start)
+t2 = threading.Thread(target=car_2.start)
+
+t1.start()
+t2.start()
