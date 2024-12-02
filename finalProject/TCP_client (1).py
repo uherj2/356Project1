dd97c9e62c34c9e26539c38a9dd72e53f04f7edc
Matt2-D <mattthedestroyer1234@gmail.com>
2024-12-02 01:37:49 -0500

    Update TCP_client (1).py

--- finalProject/TCP_client (1).py
+++ finalProject/TCP_client (1).py
@@ -6,34 +6,55 @@ import socket
 import sys
 import time
 import threading
+import json
+
 data = "Recieved:".encode('utf-8')
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
-    s.connect((sys.argv[1], int(sys.argv[2])))
-    #s.connect(('insert IP', insert port number))
+    #s.connect((sys.argv[1], int(sys.argv[2])))
+    s.connect(('172.18.112.1', 54321))
 except TimeoutError:
     print('Timed out connecting to server.')
 except Exception as e:
     print(f'Exception while setting up socket and connecting: {e}')
 else:
     try:
-        running = True
-             while running == True:
+         loggedIn = False
+         while loggedIn == False:
 
-                msg = s.recv(256).decode('utf-8')
-                while msg and msg[-1] != '\n':
-                    msg += s.recv(256).decode('utf-8')
+            msg = s.recv(256).decode('utf-8')
 
-                print(msg)
-                if msg.strip() == "STOP":
-                    print("Ending Process")
-                    running = False
-                elif msg.strip()[len(msg.strip())-1] == ':':
-                    inp1 = input(">> ")
-                    s.sendall(f'{inp1}\n'.encode('utf-8'))
+            print(msg)
+            if msg.strip() == "Successfully signed into server":
+                loggedIn = True
+            elif msg.strip()[len(msg.strip())-1] == ':':
+                inp1 = input(">> ")
+                s.send(f'{inp1}\n'.encode('utf-8'))
 
+
+         running = True
+         while running == True:
+            #read last message
+            time.sleep(1)
+            msg = s.recv(256).decode('utf-8')
+            print(msg)
+
+            rec = input("Enter recipient or type 'STOP':\n")
+            if rec == "STOP":
+                data = {"action": "stop"}
+                json_packet = json.dumps(data)
+                s.send(f'{json_packet}\n'.encode('utf-8'))
+                running == False
+                print("All Done!")
+                break
+            else:
+                msg = input("Enter Message:\n")
+                data = {"action":"message","recipient":rec,"message":msg}
+                json_packet = json.dumps(data)
+                s.send(f'{json_packet}\n'.encode('utf-8'))
+                #checks for message
 
     except Exception as e:
         print(f'Exception sending: {e}')
