e3bf145c8c7d0cac5c460757d0bf61cfb8fe0f0a
Matt2-D <mattthedestroyer1234@gmail.com>
2024-12-02 00:38:06 -0500

    TCP_client (1).py

--- finalProject/TCP_client (1).py
+++ finalProject/TCP_client (1).py
@@ -12,24 +12,27 @@ try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
     s.connect((sys.argv[1], int(sys.argv[2])))
-    #s.connect(('172.18.112.1', 54321))
+    #s.connect(('insert IP', insert port number))
 except TimeoutError:
     print('Timed out connecting to server.')
 except Exception as e:
     print(f'Exception while setting up socket and connecting: {e}')
 else:
     try:
+        running = True
+             while running == True:
 
-         while True:
+                msg = s.recv(256).decode('utf-8')
+                while msg and msg[-1] != '\n':
+                    msg += s.recv(256).decode('utf-8')
 
-            msg = s.recv(256).decode('utf-8')
-            while msg and msg[-1] != '\n':
-                msg += s.recv(256).decode('utf-8')
-
-            print(msg)
-            if msg.strip()[len(msg.strip())-1] == ':':
-                inp1 = input(">> ")
-                s.sendall(f'{inp1}\n'.encode('utf-8'))
+                print(msg)
+                if msg.strip() == "STOP":
+                    print("Ending Process")
+                    running = False
+                elif msg.strip()[len(msg.strip())-1] == ':':
+                    inp1 = input(">> ")
+                    s.sendall(f'{inp1}\n'.encode('utf-8'))
 
 
     except Exception as e:
